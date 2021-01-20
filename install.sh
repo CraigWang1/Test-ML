3e479eaf972a057574aee273d7ee799e24e9fa72
Craigwang1 <Craigwang1@gmail.com>
2021-01-20 08:28:04 -0800

    Fix install script

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo apt-get install python3-opencv
 
 # Install tensorflow for C
 wget https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz 
-tar -C /usr/local -xzf libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz  
+sudo tar -C /usr/local -xzf libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz  
 git clone https://github.com/serizba/cppflow.git          
 sudo ldconfig     
