6644f9d869a9f1cb9062fad784eb0ddb3df77b82
Craigwang1 <Craigwang1@gmail.com>
2021-01-20 08:11:22 -0800

    test

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+# Install OpenCV
 sudo apt update
-cd /tmp
+sudo apt-get install python3-opencv
 
 # Install tensorflow for C
 wget https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz 
-tar -C /usr/local -xzf /tmp/libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz  
+tar -C /usr/local -xzf libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz  
 git clone https://github.com/serizba/cppflow.git          
 sudo ldconfig     
-
-# Install OpenCV
-sudo apt-get install python3-opencv
